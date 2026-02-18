767d5b58027e5ba2a0dde266ddd9b22d32f7eaf1
AnaClaudiaIR <anac.ihnrod@educa.jcyl.es>
2026-02-18 12:03:56 +0100

    subir2

--- src/MainArrayListNombre.java
+++ src/MainArrayListNombre.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
 
-public class MainArrayListNombre {
-}
+import java.util.ArrayList;
+import java.util.List;
+
+    public class MainArrayListNombre{
+        public static void main(String[] args) {
+            //1. Creación de la lista
+            List<String> lista = new ArrayList<>();
+
+            //2. Añadir elementos
+            lista.add("Elemento1");
+            lista.add("Elemento2");
+            lista.add("Elemento2");
+
+            lista.add(3,"Elemento3");
+            lista.add(4,"Elemento4");
+
+            //3. Mostrar elementos de la lista
+            for (String elemento:lista){
+                System.out.println(elemento);
+            }
+
+            //4. Eliminar elementos de la lista
+            lista.remove(4);
+            lista.remove("Elemento1");
+
+            System.out.println("-----------------------------");
+            for (String elemento:lista){
+                System.out.println(elemento);
+            }
+        }
+    }
+
